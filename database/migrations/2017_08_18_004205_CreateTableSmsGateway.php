0c42bf5e73baeda3aa3aec008a145c6dcf820634
Ilfan Zulkarnain <ilfan.highker@gmail.com>
2017-08-19 00:16:15 +0700

    Disable kelompok sms

--- database/migrations/2017_08_18_004205_CreateTableSmsGateway.php
+++ database/migrations/2017_08_18_004205_CreateTableSmsGateway.php
@@ -18,6 +18,7 @@ class CreateTableSmsGateway extends Migration
             $table->string('notujuan');
             $table->string('isipesan');
             $table->enum('status', ['Terkirim', 'Pending'])->default('Pending');
+            // disable temporary
             // $table->integer('kelompok_sms')->unsigned()->nullable();
             $table->timestamps();
         });
