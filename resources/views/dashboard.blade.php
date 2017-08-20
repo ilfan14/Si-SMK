d5d6bfa06734dd82e83cdf6aefea306b3a77bc50
Ilfan Zulkarnain <ilfan.highker@gmail.com>
2017-08-20 23:50:16 +0700

    Temporary Hiden

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -74,7 +74,7 @@
                                 <i class="livicon  pull-right" data-name="eye-open" data-l="true" data-c="#fff"
                                    data-hc="#fff" data-s="70"></i>
                             </div>
-                            <div class="row">
+                            <div hidden class="row">
                                 <div class="col-xs-6">
                                     <small class="stat-label">Last Week</small>
                                     <h4 id="myTargetElement1.1"></h4>
@@ -88,7 +88,7 @@
                     </div>
                 </div>
             </div>
-            <!-- <div class="col-lg-3 col-md-6 col-sm-6 margin_10 animated fadeInUpBig">
+            <div hidden class="col-lg-3 col-md-6 col-sm-6 margin_10 animated fadeInUpBig">
                 <div class="redbg no-radius">
                     <div class="panel-body squarebox square_boxs">
                         <div class="col-xs-12 pull-left nopadmar">
@@ -114,8 +114,8 @@
                         </div>
                     </div>
                 </div>
-            </div> -->
-            <!-- <div class="col-lg-3 col-sm-6 col-md-6 margin_10 animated fadeInDownBig">
+            </div>
+            <div hidden class="col-lg-3 col-sm-6 col-md-6 margin_10 animated fadeInDownBig">
                 <div class="goldbg no-radius">
                     <div class="panel-body squarebox square_boxs">
                         <div class="col-xs-12 pull-left nopadmar">
@@ -141,7 +141,7 @@
                         </div>
                     </div>
                 </div>
-            </div> -->
+            </div>
             <div class="col-lg-3 col-md-6 col-sm-6 margin_10 animated fadeInRightBig">
                 <!-- Trans label pie charts strats here-->
                 <div class="palebluecolorbg no-radius">
@@ -156,7 +156,7 @@
                                 <i class="livicon pull-right" data-name="users" data-l="true" data-c="#fff"
                                    data-hc="#fff" data-s="70"></i>
                             </div>
-                            <div class="row">
+                            <div hidden class="row">
                                 <div class="col-xs-6">
                                     <small class="stat-label">Last Week</small>
                                     <h4 id="myTargetElement4.1"></h4>
